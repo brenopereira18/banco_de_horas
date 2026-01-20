427657e792c6c09782bf7796078beca4477ed80c
brenopereira18 <brenopereira@gmail.com>
2026-01-20 08:32:54 -0300

    raw: setup do projeto

---
+++ src/main/java/com/banco_de_horas/banco_de_horas/BancoDeHorasApplication.java
@@ -0,0 +1,13 @@
+package com.banco_de_horas.banco_de_horas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoDeHorasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoDeHorasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banco_de_horas/banco_de_horas/BancoDeHorasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco_de_horas.banco_de_horas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoDeHorasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
