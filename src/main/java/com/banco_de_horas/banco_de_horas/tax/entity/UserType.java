f07f60329c642aff6a503659cd382c5a23ba9339
brenopereira18 <brenopereira@gmail.com>
2026-01-20 17:33:10 -0300

    feat: cria record com tipos de usuário

---
+++ src/main/java/com/banco_de_horas/banco_de_horas/tax/entity/UserType.java
@@ -0,0 +1,17 @@
+package com.banco_de_horas.banco_de_horas.tax.entity;
+
+public enum UserType {
+    FISCAL("Fiscal"),
+    SUPERVISOR("SUpervisor"),
+    ADMINISTRADOR("Administrador");
+
+    private String userType;
+
+    UserType(String userType) {
+        this.userType = userType;
+    }
+
+    public String getUserType() {
+        return this.userType;
+    }
+}
