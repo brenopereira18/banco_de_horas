0828a3c0e69fb98e8245193e14a8d0431f8ca265
brenopereira18 <brenopereira@gmail.com>
2026-01-27 11:17:58 -0300

    feat: record com os dados de resumo no mês

---
+++ src/main/java/com/banco_de_horas/banco_de_horas/timeOffUsage/dto/MonthLyTimeOffSummaryDTO.java
@@ -0,0 +1,9 @@
+package com.banco_de_horas.banco_de_horas.timeOffUsage.dto;
+
+import java.math.BigDecimal;
+
+public record MonthLyTimeOffSummaryDTO(
+    Long totalTimeOffs,
+    BigDecimal totalHoursUsed
+) {
+}
