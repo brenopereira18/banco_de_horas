cfe20a9308370b3874f01bc6814ef024e5fe4a61
brenopereira18 <brenopereira@gmail.com>
2026-01-28 11:27:19 -0300

    feat: pega os dados de saldo de horas do fiscal no get e método post para editar e excluir serviço

--- src/main/java/com/banco_de_horas/banco_de_horas/controller/DashboardController.java
+++ src/main/java/com/banco_de_horas/banco_de_horas/controller/DashboardController.java
@@ -8,14 +8,19 @@ import com.banco_de_horas.banco_de_horas.tax.entity.TaxEntity;
 import com.banco_de_horas.banco_de_horas.tax.service.TaxService;
 import com.banco_de_horas.banco_de_horas.timeOffUsage.dto.MonthLyTimeOffSummaryDTO;
 import com.banco_de_horas.banco_de_horas.timeOffUsage.service.TimeOffUsageService;
+import com.banco_de_horas.banco_de_horas.utils.TimeFormatUtils;
 import com.banco_de_horas.banco_de_horas.work.dto.MonthLyWorkSummaryDTO;
+import com.banco_de_horas.banco_de_horas.work.dto.MonthlyWorkItemDTO;
+import com.banco_de_horas.banco_de_horas.work.dto.WorkRequestDTO;
 import com.banco_de_horas.banco_de_horas.work.service.WorkService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
@@ -50,25 +55,67 @@ public class DashboardController {
     public String showFiscalDetails(@PathVariable Long id, Model model) {
         try {
             TaxEntity tax = taxService.findById(id);
-            MonthLyWorkSummaryDTO WorkSummary = workService.getMonthLySummary(tax);
+            MonthLyWorkSummaryDTO workSummary = workService.getMonthLySummary(tax);
+            List<MonthlyWorkItemDTO> works = workService.getMonthlyWorks(tax);
             MonthLyTimeOffSummaryDTO  timeOffSummary = timeOffUsageService.getMonthLySummary(tax);
 
-            model.addAttribute("timeOffSummary", timeOffSummary);
-            model.addAttribute("WorkSummary", WorkSummary);
+            model.addAttribute("formattedBalance", TimeFormatUtils.formatHours(tax.getBalanceOfHours()));
+            model.addAttribute("timeOffSummaryPerMonth", TimeFormatUtils.formatHours(timeOffSummary.totalHoursUsed()));
+            model.addAttribute("NumberTimeOffSummaryIntheMonth", timeOffSummary.totalTimeOffs());
+            model.addAttribute("workingHoursPerMonth", TimeFormatUtils.formatHours(workSummary.totalHoursGenerated()));
+            model.addAttribute("numberOfServicesInTheMonth", workSummary.totalServices());
             model.addAttribute("fiscal", tax);
+            model.addAttribute("works", works);
             model.addAttribute("userName", "Eduardo");
-
-            // Adicionar dados específicos do dashboard do fiscal (se houver)
-            // List<ServiceEntity> services = serviceService.findByFiscalId(id);
-            // List<LeaveEntity> leaves = leaveService.findByFiscalId(id);
-            // model.addAttribute("services", services);
-            // model.addAttribute("leaves", leaves);
 
             return "dashboardTax";
 
         } catch (Exception e) {
             return "redirect:/banco_de_horas/dashboard/administrador?error=FiscalNotFound";
         }
+    }
+
+    @PostMapping("/editar/servico")
+    public String editWork(@ModelAttribute WorkRequestDTO workRequestDTO, @RequestParam Long id, RedirectAttributes redirectAttributes) {
+        try {
+            workService.update(id, workRequestDTO);
+            redirectAttributes.addFlashAttribute("successMessage",
+                "Feriado atualizado com sucesso!");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("errorMessage",
+                "Erro ao atualizar feriado: " + e.getMessage());
+        }
+        return "redirect:/banco_de_horas/dashboard/fiscal/" + id;
+    }
+
+    @PostMapping("/excluir/servico/{id}")
+    public String deleteWork(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        try {
+            workService.delete(id);
+            redirectAttributes.addFlashAttribute("successMessage",
+                "Feriado excluído com sucesso!");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("errorMessage",
+                "Erro ao excluir feriado: " + e.getMessage());
+        }
+        return "redirect:/banco_de_horas/dashboard/fiscal/" + id;
+    }
+
+    @PostMapping("/fiscal/{id}/work")
+    public String addService(@PathVariable("id") Long taxId, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDateTime, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDateTime, @RequestParam String description, RedirectAttributes redirectAttributes) {
+        try {
+            WorkRequestDTO dto = new WorkRequestDTO(taxId, description, startDateTime, endDateTime);
+            workService.create(dto);
+
+            redirectAttributes.addFlashAttribute("successMessage",
+                "Serviço cadastrado com sucesso!");
+
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("errorMessage",
+                "Erro ao cadastrar serviço: " + e.getMessage());
+        }
+
+        return "redirect:/banco_de_horas/dashboard/fiscal/" + taxId;
     }
 
     @PostMapping("/criar/fiscal")
