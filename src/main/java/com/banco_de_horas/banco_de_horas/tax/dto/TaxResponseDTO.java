33cc6cde235b30f26d825490a1b83d0ad3c63208
brenopereira18 <brenopereira@gmail.com>
2026-02-02 20:34:44 -0300

    feat: criar dto para dados de listagem de fiscais

---
+++ src/main/java/com/banco_de_horas/banco_de_horas/tax/dto/TaxResponseDTO.java
@@ -0,0 +1,10 @@
+package com.banco_de_horas.banco_de_horas.tax.dto;
+
+import java.math.BigDecimal;
+
+public record TaxResponseDTO(
+    Long id,
+    String fullName,
+    String balanceOfHours
+) {
+}
