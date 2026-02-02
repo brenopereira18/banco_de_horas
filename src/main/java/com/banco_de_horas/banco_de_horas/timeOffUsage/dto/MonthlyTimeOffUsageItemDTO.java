f9d263861b8945e27f79351e5e22cb1cffd87292
brenopereira18 <brenopereira@gmail.com>
2026-02-02 08:56:57 -0300

    feat: record com os campos que serão listados em folgas

---
+++ src/main/java/com/banco_de_horas/banco_de_horas/timeOffUsage/dto/MonthlyTimeOffUsageItemDTO.java
@@ -0,0 +1,14 @@
+package com.banco_de_horas.banco_de_horas.timeOffUsage.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record MonthlyTimeOffUsageItemDTO(
+    Long id,
+    LocalDate startDate,
+    LocalDate endDate,
+    BigDecimal hoursUsed,
+    BigDecimal fractionalHours,
+    String formattedHours
+) {
+}
