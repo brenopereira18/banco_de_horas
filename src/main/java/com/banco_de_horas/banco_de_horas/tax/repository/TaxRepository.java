888168fb5eb734a5d4ad64a9a95580192133d529
brenopereira18 <brenopereira@gmail.com>
2026-02-03 09:51:35 -0300

    feat: buscar usuário pela matrícula

--- src/main/java/com/banco_de_horas/banco_de_horas/tax/repository/TaxRepository.java
+++ src/main/java/com/banco_de_horas/banco_de_horas/tax/repository/TaxRepository.java
@@ -3,7 +3,11 @@ package com.banco_de_horas.banco_de_horas.tax.repository;
 import com.banco_de_horas.banco_de_horas.tax.entity.TaxEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TaxRepository extends JpaRepository<TaxEntity, Long> {
 
     boolean existsByRegistration(String registration);
+
+    Optional<TaxEntity> findByRegistration(String registration);
 }
