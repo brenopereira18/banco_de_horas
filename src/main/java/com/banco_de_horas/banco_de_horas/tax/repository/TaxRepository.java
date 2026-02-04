3cae91b474ebcbea5fd6857ad47ada357274f897
brenopereira18 <brenopereira@gmail.com>
2026-02-04 10:22:31 -0300

    feat: busca lista de fiscais em ordem alfabética

--- src/main/java/com/banco_de_horas/banco_de_horas/tax/repository/TaxRepository.java
+++ src/main/java/com/banco_de_horas/banco_de_horas/tax/repository/TaxRepository.java
@@ -3,6 +3,7 @@ package com.banco_de_horas.banco_de_horas.tax.repository;
 import com.banco_de_horas.banco_de_horas.tax.entity.TaxEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TaxRepository extends JpaRepository<TaxEntity, Long> {
@@ -10,4 +11,6 @@ public interface TaxRepository extends JpaRepository<TaxEntity, Long> {
     boolean existsByRegistration(String registration);
 
     Optional<TaxEntity> findByRegistration(String registration);
+
+    List<TaxEntity> findAllByOrderByFullNameAsc();
 }
