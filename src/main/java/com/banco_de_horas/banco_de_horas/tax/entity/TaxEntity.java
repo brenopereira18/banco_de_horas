89450981f7cba6170d8ce5cc0cf3c3b37da9dceb
brenopereira18 <brenopereira@gmail.com>
2026-02-04 08:25:10 -0300

    refactor: retorna o papel do usuário

--- src/main/java/com/banco_de_horas/banco_de_horas/tax/entity/TaxEntity.java
+++ src/main/java/com/banco_de_horas/banco_de_horas/tax/entity/TaxEntity.java
@@ -11,6 +11,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.math.BigDecimal;
@@ -97,7 +98,9 @@ public class TaxEntity implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return List.of(
+            new SimpleGrantedAuthority("ROLE_" + userType.name())
+        );
     }
 
     @Override
