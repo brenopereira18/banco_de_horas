828256b071c86f540b67aa03de0c63e1798fced7
brenopereira18 <brenopereira@gmail.com>
2026-02-04 10:47:38 -0300

    feat: configura banco de dados para teste

--- src/test/java/com/banco_de_horas/banco_de_horas/BancoDeHorasApplicationTests.java
+++ src/test/java/com/banco_de_horas/banco_de_horas/BancoDeHorasApplicationTests.java
@@ -2,8 +2,10 @@ package com.banco_de_horas.banco_de_horas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BancoDeHorasApplicationTests {
 
 	@Test
