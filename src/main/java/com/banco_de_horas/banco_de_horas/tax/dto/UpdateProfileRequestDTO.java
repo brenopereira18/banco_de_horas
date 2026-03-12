e1a2df17fbca8b8547eb14c803fb87ac26e69ead
brenopereira18 <brenopereira@gmail.com>
2026-03-12 09:17:44 -0300

    refactor: refatora lógica para atualizar dados do perfil do usuário

--- src/main/java/com/banco_de_horas/banco_de_horas/tax/dto/UpdateProfileRequestDTO.java
+++ src/main/java/com/banco_de_horas/banco_de_horas/tax/dto/UpdateProfileRequestDTO.java
@@ -1,6 +1,8 @@
 package com.banco_de_horas.banco_de_horas.tax.dto;
 
 public record UpdateProfileRequestDTO(
-    String password
+    String email,
+    String password,
+    String passwordConfirmation
 ) {
 }
