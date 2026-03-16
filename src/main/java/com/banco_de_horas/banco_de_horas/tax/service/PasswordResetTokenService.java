8716cb55ec30634be43557bbaa194dd9a5159a21
brenopereira18 <brenopereira@gmail.com>
2026-03-16 09:28:48 -0300

    refactor: recria arquivo

--- src/main/java/com/banco_de_horas/banco_de_horas/tax/service/PasswordResetTokenService.java
+++ src/main/java/com/banco_de_horas/banco_de_horas/tax/service/PasswordResetTokenService.java
@@ -11,7 +11,6 @@ import io.github.bucket4j.Refill;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.DigestUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
