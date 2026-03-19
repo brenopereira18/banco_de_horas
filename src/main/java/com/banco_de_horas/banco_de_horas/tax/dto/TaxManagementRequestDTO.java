2f6d77e309dbabb372f263d3b6567c788c594c68
brenopereira18 <brenopereira@gmail.com>
2026-03-19 12:00:06 -0300

    feat: adiciona campo

--- src/main/java/com/banco_de_horas/banco_de_horas/tax/dto/TaxManagementRequestDTO.java
+++ src/main/java/com/banco_de_horas/banco_de_horas/tax/dto/TaxManagementRequestDTO.java
@@ -3,7 +3,8 @@ package com.banco_de_horas.banco_de_horas.tax.dto;
 import com.banco_de_horas.banco_de_horas.tax.entity.UserType;
 
 public record TaxManagementRequestDTO(
-    String fullName,
-    UserType userType
+    UserType userType,
+    String email,
+    String fullName
 ) {
 }
